d6938b8c26ae8f514e6fd250bd2f741b64d890a5
davepotvin <157930131+davepotvin@users.noreply.github.com>
2024-10-13 10:25:40 -0400

    beginning of development

---
+++ data_import.py
@@ -0,0 +1,42 @@
+import sqlite3
+import pandas as pd
+from main import *
+
+# taking a one big table approach here to keep things simple, no normalization
+
+# tester function to go straight from mock data in csv into db, will remove once I've got real data
+def insert_csv_data(csv_file):
+    conn = connect_db()
+    cur = conn.cursor()
+    data = pd.read_csv(csv_file)
+    
+    for _, row in data.iterrows():
+        cur.execute('''
+            INSERT INTO stats (year, stat_type, stat_value, source)
+            VALUES (?, ?, ?, ?)
+        ''', (row['year'], row['stat_type'], row['stat_value'], row['source']))
+    
+    conn.commit()
+    conn.close()
+    
+insert_csv_data('data/infant_mortality_rate.csv')
+
+# helper function that will insert into db once data has been cleaned up
+def insert_csv_data(df):
+    conn = connect_db()
+    cur = conn.cursor()
+    
+    for _, row in df.iterrows():
+        cur.execute('''
+            INSERT INTO stats (year, stat_type, stat_value, source)
+            VALUES (?, ?, ?, ?)
+        ''', (row['year'], row['stat_type'], row['stat_value'], row['source']))
+    
+    conn.commit()
+    conn.close()
+
+# need a cleaner function for each csv that will transform then insert the data
+def process_infant_mortality(path_to_csv):
+    df = pd.read_csv(path_to_csv)
+    
+    insert_csv_data(df)
--- main.py
+++ main.py
@@ -1,1 +1,29 @@
 import sqlite3
+import pandas as pd
+
+def connect_db():
+    return sqlite3.connect('humanity_scorecard.db')
+
+# uncomment to delete table so the next chunk refresh
+# conn = connect_db()
+# conn.execute('DROP TABLE stats')
+
+def create_table():
+    conn = connect_db()
+    cur = conn.cursor()
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS stats (
+            stats_id INTEGER PRIMARY KEY,
+            year INTEGER,
+            stat_type TEXT,
+            stat_value REAL,
+            source TEXT
+        ) 
+    ''')
+    conn.commit()
+    conn.close()
+
+if __name__ == "__main__":
+    connect_db()
+    create_table()
+    
